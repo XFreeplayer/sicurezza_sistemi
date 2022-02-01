3d7728fc314288815cb638d5a80cfa8eff941815
ecoyocena <enricoa@outlook.it>
2022-02-01 13:09:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ function oro() {
     var b=a.getHours();
     var c=a.getMinutes();
     var d=a.getSeconds();
-    document.getElementById("scr").innerHTML=( b + ":"+ c + ":" + d);  
+    
     
     if (d<10 && d>0) {
         secondi=("0" + a);
         document.getElementById("scr").innerHTML=( b + ":"+ c + ":" + secondi);  
 
 
+    }
+
+    else {
+        document.getElementById("scr").innerHTML=( b + ":"+ c + ":" + d);  
     }
 }
 setInterval(oro, 1000);
