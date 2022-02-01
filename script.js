7c2df0c58b5e8f97f8242e4e6aee70889eac00a9
XFreeplayer <98397911+XFreeplayer@users.noreply.github.com>
2022-02-01 02:03:20 +0100

    pp

--- script.js
+++ script.js
@@ -6,15 +6,32 @@ function oro() {
     var d=a.getSeconds();
     
     
-    if (d<10 && d>0) {
-        secondi=("0" + a);
-        document.getElementById("scr").innerHTML=( b + ":"+ c + ":" + secondi);  
-
-
+    if (b<10 && c<10 && d<10){ 
+        document.getElementById("scr").innerHTML=("0"+ b + ":0"+ c + ":0" + d);
+    } 
+    
+    else if (d<10 && c<10) {
+        document.getElementById("scr").innerHTML=( b + ":0"+ c + ":0" + d);
     }
-
+    
+    else if (b<10 && c<10) {
+        document.getElementById("scr").innerHTML=("0"+ b + ":0"+ c + ":" + d);
+    }
+    else if (b<10 && d<10) {
+        document.getElementById("scr").innerHTML=("0"+ b + ":"+ c + ":0" + d);
+    }
+    else if (d<10) {
+        document.getElementById("scr").innerHTML=( b + ":"+ c + ":0" + d);  
+    }
+    else if (c<10) {
+        document.getElementById("scr").innerHTML=( b + ":0"+ c + ":" + d);
+    }
+    else if (b<10) {
+        document.getElementById("scr").innerHTML=("0"+ b + ":"+ c + ":" + d);
+    }
     else {
         document.getElementById("scr").innerHTML=( b + ":"+ c + ":" + d);  
     }
+    
 }
 setInterval(oro, 1000);
